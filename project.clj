95273d5cb9a16de90db419d54bdade7f28e4708c
Martin Jul <martin@mjul.com>
2022-03-23 13:44:08 +0100

    Upgrade to Typed Clojure 1.0.21

--- project.clj
+++ project.clj
@@ -2,9 +2,10 @@
   :description "Experiments with more typing in Clojure"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/data.json "2.4.0"]
-                 [org.typedclojure/typed.clj.checker "1.0.19"]
-                 [org.typedclojure/typed.clj.runtime "1.0.19"]]
-  :plugins [[lein-typed "0.4.6"]]
+                 [org.typedclojure/typed.clj.checker "1.0.21"]
+                 [org.typedclojure/typed.clj.runtime "1.0.21"]]
+  :plugins [[lein-typed "0.4.6"]
+            [lein-ancient "1.0.0-RC4-SNAPSHOT"]]
   :core.typed {:check [clojure-types-lab.typed]}
   :repl-options {:init-ns clojure-types-lab.typed})
  
