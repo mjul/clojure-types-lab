041171f428e62245a0a5010d168d3ddc56613425
Martin Jul <martin@mjul.com>
2022-07-18 13:50:11 +0200

    Bump to Clojure 1.11.1 and typed 1.0.31

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clojure-types-lab "0.1.0-SNAPSHOT"
   :description "Experiments with more typing in Clojure"
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]
-                 [org.typedclojure/typed.clj.checker "1.0.21"]
-                 [org.typedclojure/typed.clj.runtime "1.0.21"]]
+                 [org.typedclojure/typed.clj.checker "1.0.31"]
+                 [org.typedclojure/typed.clj.runtime "1.0.31"]]
   :plugins [[lein-typed "0.4.6"]
             [lein-ancient "1.0.0-RC4-SNAPSHOT"]]
   :core.typed {:check [clojure-types-lab.typed]}
