7d8fbde2312a38790664325a9c33e19a76cf4473
Martin Jul <martin@mjul.com>
2022-07-22 11:37:03 +0200

    Add issue about type-checking latent filters on and

--- src/clojure_types_lab/typed_issues.clj
+++ src/clojure_types_lab/typed_issues.clj
@@ -1,6 +1,6 @@
 (ns clojure-types-lab.typed-issues
   {:lang :core.typed}
-  (:require 
+  (:require
    [clojure.set :as set]
    [typed.clojure :as t]))
 
@@ -47,12 +47,11 @@
   ;  [t/Any -> Boolean :filters {:then (is (t/Map t/Any t/Any) 0), :else tt}]
   ;  [t/Any -> Boolean :filters {:then (is (t/Map t/Any t/Any) 0), :else tt}])
   ; {:then tt, :else ff}]
-
   )
 
 
 (comment
-  
+
   ;; clojure.core/tree-seq is not annotated:
   (t/cf (tree-seq map?
                   :children
@@ -67,5 +66,45 @@
   ; ; 
   ; in:
   ; tree-seq
-
   )
+
+;; ----------------------------------------------------------------
+;; Combining latent filters for predicates in and form is 
+;; currently weaker than necessary:
+
+(t/defalias PartyId Long)
+(t/ann party-id? (t/Pred PartyId))
+(defn party-id? [x]
+  ^::t/dbg
+  (and
+   ^::t/dbg
+   (instance? Long x)
+   ^::t/dbg
+   (pos-int? x)))
+
+;; The type-checker says this function has the type
+;; => [(Var
+;;      [t/Any -> Boolean :filters {:then (is Long 0), :else tt}]
+;;      [t/Any -> Boolean :filters {:then (is Long 0), :else tt}])
+;;     {:then tt, :else ff}]
+
+;; Somehow it does not capture that the fact that we know that :else (! Long x) after the (instance? ...) form.
+;; So the :else filter becomes :tt instead of narrowing down the filter to :else (! Long 0)
+
+;; The debug tagging prints this:
+
+;;  ::t/dbg id=G__52873 (and (instance? Long x) (pos-int? x))
+;;  ::t/dbg id=G__52873 expected: Boolean
+;;   ::t/dbg id=G__52874 (instance? Long x)
+;;   ::t/dbg id=G__52874 result: [(t/U false true) {:then (is Long x__#0), :else (! Long x__#0)}]
+;;   ::t/dbg id=G__52875 (pos-int? x)
+;;   ::t/dbg id=G__52875 expected: Boolean
+;;   ::t/dbg id=G__52875 result: [Boolean {:then (is (t/U Short Byte Long Integer) x__#0), :else tt}]
+;;  ::t/dbg id=G__52873 result: [Boolean {:then (is Long x__#0), :else tt}]
+;;  ; Type Error (file:/C:/Users/marti/src/github/mjul/clojure-types-lab/src/clojure_types_lab/typed_issues.clj:77:1) 
+;;  ; Expected result with filter {:then (is clojure-types-lab.typed-issues/PartyId x__#0), :else (! clojure-types-lab.typed-issues/PartyId x__#0)}, got filter {:then (is Long x__#0), :else tt}
+;;  ; 
+;;  ; 
+;;  in:
+;;  (fn* ([x] (and (instance? Long x) (pos-int? x))))
+
